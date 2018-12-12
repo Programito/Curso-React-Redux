eae926357f7e7f041bd04ad13f347cc75c2781ae
Programito <Programito@gmail.com>
2018-12-12 18:43:36 +0100

    react-flexbox-grid

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
-import  MuiThemeProvider  from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import LocationList from './components/LocationList';
+import ForecastExtented from './components/ForecastExtented';
 import './App.css';
 
-const cities= [
-          'Buenos Aires,ar',
-          'Tokyo,JP',
-          'Barcelona,es',
-          'London,GB',
-          'Washington DC.,US' 
+const cities = [
+  'Buenos Aires,ar',
+  'Tokyo,JP',
+  'Barcelona,es',
+  'London,GB',
+  'Washington DC.,US'
 ];
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {city: 'Nueva Ciudad'};
+  }
+
+  handleSelectedLocation = city => {
+    this.setState({city});
+    console.log(`handleSelectedLocation ${city}`);
+  }
+
   render() {
+    const {city} = this.state;
     return (
+
       <MuiThemeProvider>
-        <div className="App">
-          <LocationList cities={cities}></LocationList>
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12} >
+              <AppBar title="WeatherApp"></AppBar>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className='detail'>
+                  <ForecastExtented city={city}></ForecastExtented>
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -47,9 +47,10 @@ class WeatherLocation extends Component {
 
     
     render = () => {
+        const {onWeatherLocationClick} = this.props;
         const {city,data} = this.state;
         return (
-        <div className="weatherLocationCont">
+        <div className="weatherLocationCont" onClick={onWeatherLocationClick}>
             <Location city={city} />
             {data ? <WeatherData data={data} /> :
                 <CircularProgress size={60} thickness={7} />}
@@ -59,6 +60,7 @@ class WeatherLocation extends Component {
 
 WeatherLocation.propTypes= {
     city:PropTypes.string,
+    onWeatherLocationClick: PropTypes.func,
 }
 
 export default WeatherLocation;
